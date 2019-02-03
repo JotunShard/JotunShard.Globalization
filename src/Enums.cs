fb79ec1ff34446f3462a747bf4280783d5f3b5d3
CodeBeast357 <270547+CodeBeast357@users.noreply.github.com>
2019-02-03 18:57:08 -0500

    Initial commit

---
+++ src/Enums.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace JotunShard.Globalization
+{
+    public static class Enums
+    {
+        public static string GetStringValue(this Enum value)
+            => ((StringEnumValuAttribute)Attribute.GetCustomAttribute(value.GetType(), typeof(StringEnumValuAttribute)))?.Value;
+    }
+}
---
+++ src/StringValueAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace JotunShard.Globalization
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class StringEnumValuAttribute : Attribute
+    {
+        public string Value { get; }
+
+        public StringEnumValuAttribute(string value)
+        {
+            Value = value;
+        }
+    }
+}
